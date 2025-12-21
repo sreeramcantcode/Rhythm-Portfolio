3d0606c2fcc64a6172129ebfffc440c27aa40484
Eighth-Horcruxxx <sreeramsunthar@gmail.com>
2025-12-21 21:10:08 +0530

    made it cooler

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,6 +33,9 @@ function Home(){
             </div>
              
              <h1 className="text-white h-90 mt-15 text-[220px] lose flex justify-center items-center text z-0 hover:text-red-500 hover:text-[240px] cursor-pointer duration-300 whitespace-nowrap">LOSE MY MIND</h1>
+             <h1 className="fixed top-2 left-2 z-[9999] text-red-500 text-xl">
+  UPDATED 🔥
+</h1>
 
              <div className=" py-30 h-70 flex justify-center mt-35 gap-20 ">
                 <img src={frame} className="w-20 h-20 object-contain relative scale-170 z-20 hover:scale-240 duration-300 cursor-pointer" alt="frame" />
